76646e019b5532f7aca344d8199dcd210e59dc1a
Mihail Nazarov <msnazarow@gmail.com>
2021-09-18 05:26:24 +0300

    feat(card)

---
+++ ex01/Card.swift
@@ -0,0 +1,14 @@
+//
+//  Card.swift
+//
+//
+//  Created by out-nazarov2-ms on 18.09.2021.
+//
+
+import Foundation
+
+class Card: NSObject, Equatable, CustomStringConvertible {
+    let color: Color
+    let value: Value
+
+}
