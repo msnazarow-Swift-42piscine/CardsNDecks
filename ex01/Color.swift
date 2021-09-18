d859877a540af068e56546dcaa619cb6cb79c24f
Mihail Nazarov <msnazarow@gmail.com>
2021-09-18 05:14:15 +0300

    feat(ex01)

---
+++ ex01/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  
+//
+//  Created by out-nazarov2-ms on 18.09.2021.
+//
+
+enum Color: String, CaseIterable {
+    case spade
+    case diamond
+    case heart
+    case clover
+
+    static let allColors = allCases
+}
---
+++ ex01/Value.swift
@@ -0,0 +1,24 @@
+//
+//  Value.swift
+//
+//
+//  Created by out-nazarov2-ms on 18.09.2021.
+//
+
+enum Value: Int, CaseIterable {
+    case two
+    case three
+    case four
+    case five
+    case six
+    case seven
+    case eight
+    case nine
+    case ten
+    case jack
+    case queen
+    case king
+    case ace
+
+    static let allValues = allCases
+}
